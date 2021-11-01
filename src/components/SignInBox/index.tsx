82ed1d5db4deaad2c74cf6223005b19677229b13
Gustavo Borges da Silva <gustavoreset3@gmail.com>
2021-11-01 18:50:53 -0300

    Primeiro Commit

---
+++ src/components/SignInBox/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { useAuth } from '../../hooks/auth';
+import {
+    View
+} from 'react-native';
+
+import { styles } from './styles';
+
+import { Button } from '../Button';
+import { COLORS } from '../../theme';
+
+export function SignInBox() {
+
+    const { signIn, isSigningIn } = useAuth();
+
+    return (
+        <View style={styles.container}>
+            <Button
+                title="ENTRAR COM O GITHUB"
+                color={COLORS.BLACK_PRIMARY}
+                backgroundColor={COLORS.YELLOW}
+                icon="github"
+                onPress={signIn}
+                isLoading={isSigningIn}
+            />
+        </View>
+    );
+}
